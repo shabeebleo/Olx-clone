2226802f0b970255d14daf3cf26db8152ef4e83e
shabeebleo <shabeebpv0786@gmail.com>
2022-10-17 15:32:06 +0530

    upto firebase context

---
+++ src/Components/Signup/Signup.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+
+import Logo from '../../olx-logo.png';
+import './Signup.css';
+
+export default function Signup() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(username);
+  }
+  return (
+    <div>
+      <div className="signupParentDiv">
+        <img width="200px" height="200px" src={Logo}></img>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="fname">Username</label>
+          <br />
+          <input
+            className="input"
+            type="text"
+            id="fname"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            name="name"
+            defaultValue="John"
+          />
+          <br />
+          <label htmlFor="fname">Email</label>
+          <br />
+          <input
+            className="input"
+            type="email"
+            id="fname"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            name="email"
+            defaultValue="John"
+          />
+          <br />
+          <label htmlFor="lname">Phone</label>
+          <br />
+          <input
+            className="input"
+            type="number"
+            id="lname"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            name="phone"
+            defaultValue="Doe"
+          />
+          <br />
+          <label htmlFor="lname">Password</label>
+          <br />
+          <input
+            className="input"
+            type="password"
+            id="lname"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            name="password"
+            defaultValue="Doe"
+          />
+          <br />
+          <br />
+          <button>Signup</button>
+        </form>
+        <a>Login</a>
+      </div>
+    </div>
+  );
+}
---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAw1LjNcHdUUandxYKw887Zyig3hLBo6FI",
+    authDomain: "fir-6dc47.firebaseapp.com",
+    projectId: "fir-6dc47",
+    storageBucket: "fir-6dc47.appspot.com",
+    messagingSenderId: "1067948252038",
+    appId: "1:1067948252038:web:5353cc16522b941a81aa07",
+    measurementId: "G-4KXTK04N0R"
+  };
+
+ export default firebase.initializeApp(firebaseConfig)
